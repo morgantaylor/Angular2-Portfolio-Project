ce9f65b87defbc16fad4aecc9c93be3a32c7ce6d
Morgan Taylor <mtwebmedia@gmail.com>
2018-11-02 09:06:21 -0700

    Bug fix: Router Scroll Position Restoration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -40,7 +40,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
+  imports: [ 
+    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'})
+  ],
   exports: [ RouterModule ]
 })
 
